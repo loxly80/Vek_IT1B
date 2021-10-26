ff26450be8b5a79c4bf360fe84af4cca9f82c1b5
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2021-10-26 10:27:17 +0200

    Add project files.

---
+++ Vek_IT1B/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vek_IT1B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
